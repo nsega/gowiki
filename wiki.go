ded3b81f2053b4f182e1d1d12554066c8256ef64
Naoki Sega <nsegaster@gmail.com>
2015-10-12 13:47:29 +0900

    Added editing func

--- wiki.go
+++ wiki.go
@@ -31,13 +31,28 @@ func viewHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "<h1>%s</h1><div>%s</div>", p.Title, p.Body)
 }
 
+func editHandler(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[len("/edit/"):]
+	p, _ := laadPage(title)
+	if err != nil {
+		p = &Page{Title: title}
+	}
+	fmt.Fprintf(w, "<h1>Editting %s</h1>"+
+		"<form action=\"/save/%s\" method=\"POST\">"+
+		"<textarea name =\"body\">%s</textarea><br>"+
+		"<input type=\"submit\" value=\"Save\">"+
+		"</form>",
+		p.Title, p.Title, p.Body)
+
+}
+
 func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
 }
 
 func main() {
-	//	http.HandleFunc("/", handler)
-	//	http.ListenAndServe(":8080", nil)
 	http.HandleFunc("/view/", viewHandler)
+	http.HandleFunc("/edit/", editHandler)
+	http.HandleFunc("/save/", saveHandler)
 	http.ListenAndServe(":8080", nil)
 }
